ce507e8629b64c211638182134964e137429d5ac
Raghav Bhatnagar <Raghav_Bhatnagar@intuit.com>
2017-01-19 01:43:36 -0800

    Solutions

--- src/Leetcode/Q003_Longest_Substring_Without_Repeating_Characters.java
+++ src/Leetcode/Q003_Longest_Substring_Without_Repeating_Characters.java
@@ -1,7 +1,32 @@
 package Leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by rbhatnagar2 on 1/12/17.
+ *
+ * Given a string, find the length of the longest substring without repeating characters.
+ *
  */
 public class Q003_Longest_Substring_Without_Repeating_Characters {
+    public int lengthOfLongestSubstring(String str) {
+        if (str.length() == 0) {
+            return 0;
+        }
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        int leftPointer=0,
+                result = 0;
+
+        for (int i=0; i<str.length(); i++) {
+            Character c = str.charAt(i);
+            if (map.containsKey(c)) {
+                leftPointer = Math.max(leftPointer, map.get(c) + 1);
+            }
+            map.put(c, i);
+            int currLength = i - leftPointer + 1;
+            result = Math.max(result, currLength);
+        }
+        return result;
+    }
 }
