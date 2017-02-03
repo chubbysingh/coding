8b4eaab4b29908f86a40d038339bb546d1c5e9b2
Raghav Bhatnagar <Raghav_Bhatnagar@intuit.com>
2017-02-02 21:55:16 -0800

    Initial commit

--- src/Leetcode/Q461_Hamming_Distance.java
+++ src/Leetcode/Q461_Hamming_Distance.java
@@ -2,6 +2,20 @@ package Leetcode;
 
 /**
  * Created by rbhatnagar2 on 1/15/17.
+ *
+ * The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+ * Given two integers x and y, calculate the Hamming distance.
  */
 public class Q461_Hamming_Distance {
+    public int hammingDistance(int x, int y) {
+        int result = 0;
+        int xor = x ^ y;
+
+        while(xor > 0) {
+            result += xor & 1;
+            xor = xor >> 1;
+        }
+        return result;
+    }
+
 }
--- src/Leetcode/Q463_Island_Perimeter.java
+++ src/Leetcode/Q463_Island_Perimeter.java
@@ -4,4 +4,28 @@ package Leetcode;
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q463_Island_Perimeter {
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+
+        for (int i=0; i < grid.length; i++) {
+            for (int j=0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+                }
+                if (grid[i+1][j] == 1) {
+                    perimeter -= 1;
+                }
+                if (grid[i-1][j] == 1) {
+                    perimeter -= 1;
+                }
+                if (grid[i][j+1] == 1) {
+                    perimeter -= 1;
+                }
+                if (grid[i][j-1] == 1) {
+                    perimeter -= 1;
+                }
+            }
+        }
+        return perimeter;
+    }
 }
