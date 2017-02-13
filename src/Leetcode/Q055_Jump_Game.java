9fac9f1c4014077574f0898a30aea911de70af29
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-02-13 01:27:50 -0800

    Solutions

--- src/Leetcode/Q055_Jump_Game.java
+++ src/Leetcode/Q055_Jump_Game.java
@@ -2,6 +2,24 @@ package Leetcode;
 
 /**
  * Created by rbhatnagar2 on 1/13/17.
+ *
+ * https://leetcode.com/problems/jump-game/
+ *
  */
 public class Q055_Jump_Game {
+    public boolean canJump(int[] nums) {
+        if (nums.length <= 1)
+            return true;
+
+        int max = nums[0];
+        for (int i=0; i<nums.length; i++) {
+            if (max < i)
+                return false;
+            max = Math.max(max, i + nums[i]);
+
+            if (max >= nums.length-1)
+                return true;
+        }
+        return false;
+    }
 }
--- src/Leetcode/Q062_Unique_Paths.java
+++ src/Leetcode/Q062_Unique_Paths.java
@@ -2,6 +2,25 @@ package Leetcode;
 
 /**
  * Created by rbhatnagar2 on 1/13/17.
+ *
+ * https://leetcode.com/problems/unique-paths/
+ *
  */
 public class Q062_Unique_Paths {
+    public int uniquePaths(int m, int n) {
+        int[][] T = new int[m][n];
+        for (int i=0; i< m; i++) {
+            T[i][0] = 1;
+        }
+        for (int i=0; i< n; i++) {
+            T[0][i] = 1;
+        }
+
+        for (int i=1; i<m; i++) {
+            for (int j=1; j<n; j++) {
+                T[i][j] = T[i-1][j] + T[i][j-1];
+            }
+        }
+        return T[m-1][n-1];
+    }
 }
