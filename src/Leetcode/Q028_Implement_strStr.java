4090ffd1aef2f82b90cd283f27ccd3b43d129c50
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-02-12 23:56:56 -0800

    Solutions

--- src/Leetcode/Q028_Implement_strStr.java
+++ src/Leetcode/Q028_Implement_strStr.java
@@ -4,4 +4,29 @@ package Leetcode;
  * Created by rbhatnagar2 on 1/12/17.
  */
 public class Q028_Implement_strStr {
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null)
+            return 0;
+        else if (needle.length() == 0)
+            return 0;
+
+        else if (haystack.length() == needle.length() && haystack.equals(needle))
+            return 0;
+
+        for (int i=0; i < haystack.length() - needle.length() + 1; i++) {
+            int indexH = i;
+            for (int j = 0; j < needle.length(); j++) {
+                if (needle.charAt(j) == haystack.charAt(indexH)) {
+                    if (j == needle.length()-1)
+                        return i;
+                    else
+                        indexH++;
+                } else {
+                    break;
+                }
+
+            }
+        }
+        return -1;
+    }
 }
--- src/Leetcode/Q036_Valid_Sudoku.java
+++ src/Leetcode/Q036_Valid_Sudoku.java
@@ -1,7 +1,73 @@
 package Leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by rbhatnagar2 on 1/12/17.
  */
 public class Q036_Valid_Sudoku {
+    public boolean isValidSudoku(char[][] board) {
+        if (board.length != 9 || board[0].length != 9)
+            return false;
+
+        return validateRowAndCols(board) && validateCube(board);
+    }
+
+    private boolean validateRowAndCols(char[][] board) {
+        for (int i=0; i<9; i++) {
+            Set<Character> rowSet = new HashSet<Character>();
+            Set<Character> colSet = new HashSet<Character>();
+            for (int j = 0; j <9; j++) {
+                if(board[i][j] != '.') {
+                    if (rowSet.contains(board[i][j]))
+                        return false;
+                    else
+                        rowSet.add(board[i][j]);
+                }
+                if(board[j][i] != '.') {
+                    if (colSet.contains(board[j][i]))
+                        return false;
+                    else
+                        colSet.add(board[j][i]);
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean validateCube(char[][] board) {
+
+        for (int bucket=0; bucket<9; bucket++) {
+            Set<Character> set = new HashSet<Character>();
+            for (int i=bucket/3 * 3; i<bucket/3 * 3 + 3; i++) {
+                for (int j=bucket%3 * 3; j<bucket%3 * 3 + 3; j++) {
+                    if (board[i][j] != '.') {
+                        if (set.contains(board[i][j]))
+                            return false;
+                        else
+                            set.add(board[i][j]);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Q036_Valid_Sudoku sol = new Q036_Valid_Sudoku();
+        char[][] input = {  {'.','.','.','.','5','.','.','1','.'},
+                            {'.','4','.','3','.','.','.','.','.'},
+                            {'.','.','.','.','.','3','.','.','1'},
+                            {'8','.','.','.','.','.','.','2','.'},
+                            {'.','.','2','.','7','.','.','.','.'},
+                            {'.','1','5','.','.','.','.','.','.'},
+                            {'.','.','.','.','.','2','.','.','.'},
+                            {'.','2','.','9','.','.','.','.','.'},
+                            {'.','.','4','.','.','.','.','.','.'}
+            };
+
+        boolean isValid = sol.isValidSudoku(input);
+        System.out.println(isValid);
+    }
 }
