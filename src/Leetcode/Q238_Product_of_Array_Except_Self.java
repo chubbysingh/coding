60764480b64f060a70fd5f911860526d7f487993
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-02-12 22:54:06 -0800

    Solutions

--- src/Leetcode/Q238_Product_of_Array_Except_Self.java
+++ src/Leetcode/Q238_Product_of_Array_Except_Self.java
@@ -4,4 +4,28 @@ package Leetcode;
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q238_Product_of_Array_Except_Self {
+    public int[] productExceptSelf(int[] nums) {
+        int left[] = new int[nums.length];
+        int right[] = new int[nums.length];
+        int res[] = new int[nums.length];
+
+        //left product
+        left[0] = 1;
+        for(int i=1; i<nums.length; i++) {
+            left[i] = left[i-1]*nums[i-1];
+        }
+
+
+        //right product
+        right[nums.length-1] = 1;
+        for(int i=nums.length-2; i>=0; i--) {
+            right[i] = right[i+1] * nums[i+1];
+        }
+
+        //product of left and right
+        for(int i=0; i < res.length; i++) {
+            res[i] = left[i]*right[i];
+        }
+        return res;
+    }
 }
