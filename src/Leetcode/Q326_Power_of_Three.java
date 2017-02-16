69f505db5e97f3a34bbbd7f2f035931df8d73079
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-02-16 02:23:30 -0800

    Solutions

--- src/Leetcode/Q326_Power_of_Three.java
+++ src/Leetcode/Q326_Power_of_Three.java
@@ -14,4 +14,21 @@ public class Q326_Power_of_Three {
         else
             return false;
     }
+
+    public boolean isPowerOfThreeIterative(int n) {
+        if (n < 1)
+            return false;
+
+        while(n > 1) {
+            if (n%3 == 0)
+                n = n/3;
+            else
+                return false;
+        }
+        return true;
+    }
+
+    public boolean isPowerOfThreeMaths(int n) {
+        return n == Math.pow(3, Math.log(n)/Math.log(3));
+    }
 }
--- src/Leetcode/Q342_Power_of_Four.java
+++ src/Leetcode/Q342_Power_of_Four.java
@@ -4,7 +4,26 @@ package Leetcode;
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q342_Power_of_Four {
+
+    // Square + 3n+1 form
     public boolean isPowerOfFour(int num) {
         return ( ( (num & (num-1)) == 0) && ( (num-1) % 3 == 0) ) ;
+    }
+
+    public boolean isPowerOfFourSimple(int num) {
+        if (num < 1)
+            return false;
+        while (num > 1) {
+            if (num%4 == 0)
+                num /= 4;
+            else
+                return false;
+        }
+        return true;
+    }
+
+    public boolean isPowerOfFourMaths(int num) {
+        return num > 0
+                && num == Math.pow(4, Math.round(Math.log(num) / Math.log(4)));
     }
 }
