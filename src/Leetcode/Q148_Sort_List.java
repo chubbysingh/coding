234ec6cf09e1f0ae4320860d578c37b407436255
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-02-18 20:34:06 -0800

    Solutions

--- src/Leetcode/Q148_Sort_List.java
+++ src/Leetcode/Q148_Sort_List.java
@@ -1,7 +1,54 @@
 package Leetcode;
 
+import Leetcode.Util.ListNode;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q148_Sort_List {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        // step 1. cut the list to two halves
+        ListNode prev = null, slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = null;
+
+        // step 2. sort each half
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+
+        // step 3. merge l1 and l2
+        return merge(l1, l2);
+    }
+
+    ListNode merge(ListNode l1, ListNode l2) {
+        ListNode l = new ListNode(0), p = l;
+
+        while (l1 != null && l2 != null) {
+            if (l1.data < l2.data) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+
+        if (l1 != null)
+            p.next = l1;
+
+        if (l2 != null)
+            p.next = l2;
+
+        return l.next;
+    }
 }
--- src/Leetcode/Q229_Majority_Element_II.java
+++ src/Leetcode/Q229_Majority_Element_II.java
@@ -1,7 +1,56 @@
 package Leetcode;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q229_Majority_Element_II {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new LinkedList<Integer>();
+
+        if (nums == null || nums.length == 0)
+            return result;
+
+        int number1=nums[0], number2=nums[0],
+                count1=0, count2=0;
+
+        int length = nums.length;
+
+        for (int i=0; i<length; i++) {
+            if (nums[i] == number1)
+                count1++;
+            else if (nums[i] == number2)
+                count2++;
+            else if (count1 == 0) {
+                number1 = nums[i];
+                count1 = 1;
+            } else if (count2 == 0) {
+                number2 = nums[i];
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+
+
+        }
+
+        count1=0; count2=0;
+
+        for (int n : nums) {
+            if (n == number1)
+                count1++;
+            else if (n == number2)
+                count2++;
+        }
+
+        if (count1 > length/3)
+            result.add(number1);
+        if (count2 > length/3)
+            result.add(number2);
+
+        return result;
+    }
 }
