4f31376d2b33d3c70650978449681270e5a5c047
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-02-19 03:34:47 -0800

    Solutions

--- src/Leetcode/Q199_Binary_Tree_Right_Side_View.java
+++ src/Leetcode/Q199_Binary_Tree_Right_Side_View.java
@@ -1,7 +1,37 @@
 package Leetcode;
 
+import Leetcode.Util.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q199_Binary_Tree_Right_Side_View {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new LinkedList<Integer>();
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i=0; i<size; i++) {
+                TreeNode node = queue.poll();
+                if (i == size-1) {
+                    result.add(node.val);
+                }
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+        }
+        return result;
+    }
 }
--- src/Leetcode/Q392_Is_Subsequence.java
+++ src/Leetcode/Q392_Is_Subsequence.java
@@ -4,4 +4,19 @@ package Leetcode;
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q392_Is_Subsequence {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0)
+            return true;
+
+        int indexS = 0, indexT = 0;
+
+        while (indexT < t.length()) {
+            if (t.charAt(indexT) == s.charAt(indexS)) {
+                indexS++;
+                if (indexS == s.length()) return true;
+            }
+            indexT++;
+        }
+        return false;
+    }
 }
