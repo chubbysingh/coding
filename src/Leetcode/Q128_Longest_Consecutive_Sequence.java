76d887f71082348f757ca21ecfe3124bee56aac8
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-02-20 02:30:10 -0800

    Solutions

--- src/Leetcode/Q128_Longest_Consecutive_Sequence.java
+++ src/Leetcode/Q128_Longest_Consecutive_Sequence.java
@@ -1,7 +1,44 @@
 package Leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q128_Longest_Consecutive_Sequence {
+    public static int longestConsecutive(int[] num) {
+        // if array is empty, return 0
+        if (num.length == 0) {
+            return 0;
+        }
+
+        Set<Integer> set = new HashSet<Integer>();
+        int max = 1;
+
+        for (int e : num)
+            set.add(e);
+
+        for (int e : num) {
+            int left = e - 1;
+            int right = e + 1;
+            int count = 1;
+
+            while (set.contains(left)) {
+                count++;
+                set.remove(left);
+                left--;
+            }
+
+            while (set.contains(right)) {
+                count++;
+                set.remove(right);
+                right++;
+            }
+
+            max = Math.max(count, max);
+        }
+
+        return max;
+    }
 }
