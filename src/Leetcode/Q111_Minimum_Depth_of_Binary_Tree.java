e6a81ac6be429f9a2f88773f777455d609a95ffd
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-03-01 06:03:48 -0800

    Solutions

--- src/Leetcode/Q111_Minimum_Depth_of_Binary_Tree.java
+++ src/Leetcode/Q111_Minimum_Depth_of_Binary_Tree.java
@@ -2,16 +2,45 @@ package Leetcode;
 
 import Leetcode.Util.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by rbhatnagar2 on 1/13/17.
  */
 public class Q111_Minimum_Depth_of_Binary_Tree {
-    public int minDepth(TreeNode root) {
+    public int minDepthRecursive(TreeNode root) {
         if (root == null) return 0;
-        else if (root.left == null) return minDepth(root.right) + 1;
-        else if (root.right == null) return minDepth(root.left) + 1;
+        else if (root.left == null) return minDepthRecursive(root.right) + 1;
+        else if (root.right == null) return minDepthRecursive(root.left) + 1;
         else {
-            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+            return Math.min(minDepthRecursive(root.left), minDepthRecursive(root.right)) + 1;
         }
+    }
+
+    public int minDepthIterative(TreeNode root) {
+        if (root == null)
+            return 0;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+
+        int min = 0;
+        while(!queue.isEmpty()) {
+            int sizeOfQueue = queue.size();
+            min++;
+
+            for (int i=0; i<sizeOfQueue; i++) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null)
+                    return min;
+                else {
+                    if (node.left != null)
+                        queue.add(node.left);
+                    if (node.right != null)
+                        queue.add(node.right);
+                }
+            }
+        }
+        return min;
     }
 }
