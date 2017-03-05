039be081f4eeea7e69f7ddbdee123956156207d9
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-03-05 14:37:11 -0800

    Initial Commit

--- src/Leetcode/Q530_Minimum_Absolute_Difference_in_BST.java
+++ src/Leetcode/Q530_Minimum_Absolute_Difference_in_BST.java
@@ -1,7 +1,27 @@
 package Leetcode;
 
+import Leetcode.Util.TreeNode;
+
 /**
  * Created by rbhatnagar2 on 3/5/17.
  */
 public class Q530_Minimum_Absolute_Difference_in_BST {
+    int result = Integer.MAX_VALUE;
+    TreeNode prev = null;
+
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        getMinimumDifference(root.left);
+
+        if (prev != null) {
+            result = Math.min(result, root.val - prev.val);
+        }
+        prev = root;
+
+        getMinimumDifference(root.right);
+
+        return result;
+    }
 }
---
+++ src/Leetcode/Q533_Lonely_Pixel_II.java
@@ -0,0 +1,7 @@
+package Leetcode;
+
+/**
+ * Created by rbhatnagar2 on 3/5/17.
+ */
+public class Q533_Lonely_Pixel_II {
+}
