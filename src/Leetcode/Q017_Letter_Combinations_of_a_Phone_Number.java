c5bde03af113c616623d5e929cfb23f1334bfc97
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-03-11 03:03:17 -0800

    Solutions

--- src/Leetcode/Q017_Letter_Combinations_of_a_Phone_Number.java
+++ src/Leetcode/Q017_Letter_Combinations_of_a_Phone_Number.java
@@ -1,7 +1,37 @@
 package Leetcode;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by rbhatnagar2 on 1/12/17.
  */
 public class Q017_Letter_Combinations_of_a_Phone_Number {
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new LinkedList<String>();
+
+        if (digits == null || digits.length() == 0) return result;
+
+        String[] dict = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        StringBuilder sb = new StringBuilder();
+        letterCombinationsHelper(digits, 0, sb, dict, result);
+
+        return result;
+    }
+
+    private void letterCombinationsHelper(String digits, int start, StringBuilder temp, String[] dict, List<String> result) {
+        if (start >= digits.length()) {
+            result.add(temp.toString());
+            return;
+        }
+
+        // char to int
+        int num = digits.charAt(start) - '0';
+        for (int i = 0; i < dict[num].length(); i++) {
+            temp.append(dict[num].charAt(i));
+            letterCombinationsHelper(digits, start + 1, temp, dict, result);
+            temp.deleteCharAt(temp.length() - 1);
+        }
+    }
 }
--- src/Leetcode/Q146_LRU_Cache.java
+++ src/Leetcode/Q146_LRU_Cache.java
@@ -1,7 +1,110 @@
 package Leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q146_LRU_Cache {
+    private int capacity;
+    private Map<Integer, Node> map;
+    private Node head;
+    private Node tail;
+    private int len;
+
+    public Q146_LRU_Cache(int capacity) {
+        map = new HashMap<Integer, Node>();
+        this.capacity = capacity;
+        len = 0;
+    }
+
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            Node temp = map.get(key);
+            removeNode(temp);
+            insertHead(temp);
+
+            return temp.val;
+        } else {
+            return -1;
+        }
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node temp = map.get(key);
+            // update the node
+            temp.val = value;
+
+            removeNode(temp);
+            insertHead(temp);
+            map.put(key, temp);
+
+        } else { // do not contain
+            Node temp = new Node(key, value);
+            if (len < capacity) {
+                len++;
+            } else if (len == capacity) {
+                map.remove(tail.key);
+                removeTail();
+            }
+            insertHead(temp);
+            map.put(key, temp);
+        }
+    }
+
+    private void removeNode(Node node) {
+        Node curNode = node;
+        Node preNode = node.pre;
+        Node nextNode = node.next;
+
+        if (preNode == null) {
+            head = head.next;
+            if (head == null) tail = head;
+            else head.pre = null;
+        } else if (nextNode == null) {
+            tail = tail.pre;
+            if (tail == null) head = tail;
+            else tail.next = null;
+        } else {
+            preNode.next = nextNode;
+            nextNode.pre = preNode;
+        }
+    }
+
+    private void insertHead(Node node) {
+        if (head == null) {
+            head = node;
+            tail = head;
+            head.pre = null;
+            tail.next = null;
+        } else {
+            node.next = head;
+            head.pre = node;
+            head = node;
+            head.pre = null;
+        }
+    }
+
+    private void removeTail() {
+        tail = tail.pre;
+        if (tail == null) head = tail;
+        else tail.next = null;
+    }
+
+    // Doubly linked list
+    class Node {
+        public int key;
+        public int val;
+        public Node pre;
+        public Node next;
+
+        Node (int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
 }
+
+
