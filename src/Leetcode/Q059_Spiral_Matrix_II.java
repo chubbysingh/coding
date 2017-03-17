bad756ef47b4f8ba11a675c01485e5939dbd22c6
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-03-17 01:37:38 -0700

    Solutions

--- src/Leetcode/Q059_Spiral_Matrix_II.java
+++ src/Leetcode/Q059_Spiral_Matrix_II.java
@@ -4,4 +4,47 @@ package Leetcode;
  * Created by rbhatnagar2 on 1/13/17.
  */
 public class Q059_Spiral_Matrix_II {
+    public int[][] generateMatrix(int n) {
+        int[][] result = new int[n][n];
+        if (n == 0) {
+            return result;
+        }
+
+        int start = 1;
+        int row = 0;
+        int col = 0;
+
+        while (n > 0) {
+            if (n == 1) {
+                result[row][col] = start;
+                break;
+            }
+
+            // Move right
+            for (int i = 0; i < n - 1; i++) {
+                result[row][col++] = start++;
+            }
+
+            // Move down
+            for (int i = 0; i < n - 1; i++) {
+                result[row++][col] = start++;
+            }
+
+            // Move left
+            for (int i = 0; i < n - 1; i++) {
+                result[row][col--] = start++;
+            }
+
+            // Move up
+            for (int i = 0; i < n - 1; i++) {
+                result[row--][col] = start++;
+            }
+
+            row++;
+            col++;
+            n -= 2;
+        }
+
+        return result;
+    }
 }
--- src/Leetcode/Q061_Rotate_List.java
+++ src/Leetcode/Q061_Rotate_List.java
@@ -1,7 +1,45 @@
 package Leetcode;
 
+import Leetcode.Util.ListNode;
+
 /**
  * Created by rbhatnagar2 on 1/13/17.
  */
 public class Q061_Rotate_List {
+    public ListNode rotateRight(ListNode head, int k) {
+        //Boundry condition
+        if(head == null) return head;
+
+        //Calculate the length
+        int length = 0;
+        ListNode node = head;
+        while(node != null){
+            node = node.next;
+            length++;
+        }
+
+        // Adjust k if its more than k
+        if (k >= length)
+            k = k % length;
+
+        // Split list to fpart and lpart
+        ListNode lpart, fpart;
+        lpart = head;
+        for(int i = 1; i < length - k; i++){
+            head = head.next;
+        }
+
+        fpart = head.next;
+        head.next = null;
+
+        // move to last element of fpart and assign next as lpart
+        if(fpart == null) return lpart;
+        node = fpart;
+        while(node.next != null){
+            node = node.next;
+        }
+        node.next = lpart;
+
+        return fpart;
+    }
 }
