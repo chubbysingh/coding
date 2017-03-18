d8e995b42c65c669e4e78328a0e30ad20f492f20
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-03-18 15:59:08 -0700

    Solutions

--- src/Leetcode/Q166_Fraction_to_Recurring_Decimal.java
+++ src/Leetcode/Q166_Fraction_to_Recurring_Decimal.java
@@ -1,7 +1,54 @@
 package Leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q166_Fraction_to_Recurring_Decimal {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0)
+            return "0";
+        StringBuilder res = new StringBuilder();
+        if ((numerator < 0 && denominator > 0) || (numerator > 0 && denominator < 0))
+            res.append("-");
+
+        numerator = Math.abs(numerator);
+        denominator = Math.abs(denominator);
+
+        //integral part
+        res.append(numerator/denominator);
+
+        int num = numerator%denominator;
+        if (num == 0)
+            return res.toString();
+
+        //decimal part
+        res.append(".");
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(num, res.length());
+        while(num != 0) {
+            num = num*10;
+            res.append(num/denominator);
+            num = num%denominator;
+            if(map.containsKey(num)) {
+                res.insert(map.get(num), "(");
+                res.append(")");
+                break;
+            } else {
+                map.put(num, res.length());
+            }
+
+        }
+        return res.toString();
+
+    }
+
+    public static void main(String[] args) {
+        Q166_Fraction_to_Recurring_Decimal sol = new Q166_Fraction_to_Recurring_Decimal();
+        String res = sol.fractionToDecimal(-1 , -2147483648);
+        System.out.println(res);
+    }
 }
--- src/Leetcode/Q200_Number_of_Islands.java
+++ src/Leetcode/Q200_Number_of_Islands.java
@@ -2,6 +2,58 @@ package Leetcode;
 
 /**
  * Created by rbhatnagar2 on 1/15/17.
+ *
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+ * An island is surrounded by water and is formed by connecting adjacent lands
+ * horizontally or vertically.
+ * You may assume all four edges of the grid are all surrounded by water.
+ *
+ * Example 1:
+ *
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ * Answer: 1
+ *
+ * Example 2:
+ *
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ * Answer: 3
+ *
  */
 public class Q200_Number_of_Islands {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0)
+            return 0;
+
+        int count = 0;
+        int rows = grid.length,
+                cols = grid[0].length;
+
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<cols; j++) {
+                if(grid[i][j] == '1') {
+                    count++;
+                    helper(grid, i, j);
+                }
+
+            }
+        }
+        return count;
+    }
+
+    private void helper(char[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] != '1')
+            return;
+
+        grid[row][col] = '0';
+        helper(grid, row+1, col);
+        helper(grid, row-1, col);
+        helper(grid, row, col+1);
+        helper(grid, row, col-1);
+    }
 }
