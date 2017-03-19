1d8da0a1de04dad9318cf66063c9a9c99b48077e
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-03-19 03:47:41 -0700

    Solutions

--- src/Leetcode/Q023_Merge_k_Sorted_Lists.java
+++ src/Leetcode/Q023_Merge_k_Sorted_Lists.java
@@ -1,5 +1,7 @@
 package Leetcode;
 
+import Leetcode.Util.ListNode;
+
 /**
  * Created by rbhatnagar2 on 1/12/17.
  *
@@ -8,4 +10,48 @@ package Leetcode;
  *
  */
 public class Q023_Merge_k_Sorted_Lists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0)
+            return null;
+
+        if (lists.length == 1)
+            return lists[0];
+
+        int numLists = lists.length;
+
+        return merge(lists, 0, numLists - 1);
+    }
+
+    ListNode merge(ListNode[] lists, int lo, int hi) {
+        if (lo == hi)
+            return lists[lo];
+
+        int mid = (lo + hi) / 2;
+
+        ListNode l1 = merge(lists, lo, mid);
+        ListNode l2 = merge(lists, mid + 1, hi);
+
+        return mergeTwoLists(l1, l2);
+    }
+
+    ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+
+        while (l1 != null && l2 != null) {
+            if (l1.data <= l2.data) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if (l1 != null) p.next = l1;
+        else if (l2 != null) p.next = l2;
+
+        return head.next;
+    }
 }
--- src/Leetcode/Q124_Binary_Tree_Maximum_Path_Sum.java
+++ src/Leetcode/Q124_Binary_Tree_Maximum_Path_Sum.java
@@ -1,7 +1,34 @@
 package Leetcode;
 
+import Leetcode.Util.TreeNode;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q124_Binary_Tree_Maximum_Path_Sum {
+    private int maxSum = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        maxPathSumHelper(root);
+        return maxSum;
+    }
+
+    private int maxPathSumHelper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxPathSumHelper(root.left);
+        int right = maxPathSumHelper(root.right);
+
+        int arch = left + right + root.val;
+
+        int pathSum = Math.max(root.val, Math.max(left, right) + root.val);
+
+        maxSum = Math.max(maxSum, Math.max(arch, pathSum));
+
+        return pathSum;
+    }
 }
