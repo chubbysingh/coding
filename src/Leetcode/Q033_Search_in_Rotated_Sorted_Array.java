99f1b77e79b0cdefc98627f720f0dca837e6660e
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-03-19 22:44:24 -0700

    Solutions

--- src/Leetcode/Q033_Search_in_Rotated_Sorted_Array.java
+++ src/Leetcode/Q033_Search_in_Rotated_Sorted_Array.java
@@ -4,6 +4,35 @@ package Leetcode;
  * Created by rbhatnagar2 on 1/12/17.
  */
 public class Q033_Search_in_Rotated_Sorted_Array {
+    public int searchIterative(int[] nums, int target) {
+        if (nums.length == 0) return -1;
+
+        int low = 0, high = nums.length-1;
+
+        while (low <= high) {
+            int mid = low + (high-low)/2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            } else {
+                if (nums[mid] < target && target <= nums[high])
+                    low = mid +1 ;
+                else
+                    high = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+
+
+    /* Recursive */
     public int search(int[] nums, int target) {
         if (nums == null || nums.length < 1)
             return -1;
--- src/Leetcode/Q081_Search_in_Rotated_Sorted_Array_II.java
+++ src/Leetcode/Q081_Search_in_Rotated_Sorted_Array_II.java
@@ -2,6 +2,47 @@ package Leetcode;
 
 /**
  * Created by rbhatnagar2 on 1/13/17.
+ *
+ * Follow up for "Search in Rotated Sorted Array":
+ * What if duplicates are allowed?
+ *
+ * Would this affect the run-time complexity? How and why?
+ *
  */
 public class Q081_Search_in_Rotated_Sorted_Array_II {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return false;
+
+        int low = 0, high = nums.length-1;
+
+        while (low <= high) {
+            int mid = low + (high-low)/2;
+
+            if (nums[mid] == target)
+                return true;
+
+            // Input contains duplicate
+            // Left is sorted for sure...
+            if (nums[low] < nums[mid] || nums[high] < nums[mid]) {
+                if (nums[low] <= target && target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            // Right is sorted for sure...
+            else if (nums[mid] < nums[high] || nums[mid] < nums[low]) {
+                if (nums[mid] < target && target <= nums[high])
+                    low = mid +1 ;
+                else
+                    high = mid - 1;
+            }
+            //Something is equal
+            else {
+                high--;
+            }
+
+        }
+        return false;
+    }
 }
