cb5194cbd2102dae55919b6f059aad861e952185
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-03-24 23:54:41 -0700

    Solutions

--- src/Leetcode/Q092_Reverse_Linked_List_II.java
+++ src/Leetcode/Q092_Reverse_Linked_List_II.java
@@ -1,5 +1,7 @@
 package Leetcode;
 
+import Leetcode.Util.ListNode;
+
 /**
  * Created by rbhatnagar2 on 1/13/17.
  *
@@ -7,4 +9,42 @@ package Leetcode;
  *
  */
 public class Q092_Reverse_Linked_List_II {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null || head.next == null || m == n) {
+            return head;
+        }
+
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next  = head;
+
+        int count = 1;
+        /// Find the tail node of the first segment
+        ListNode firstTail = dummyNode;
+        while (count < m) {
+            firstTail = firstTail.next;
+            count++;
+        }
+
+        /// reverse the middle segement
+        ListNode midTail = firstTail.next;
+        ListNode midHead = firstTail.next;
+
+        ListNode prev = null;
+        int num = n - m + 1;
+        int i = 0;
+        while (i < num) {
+            ListNode next = midHead.next;
+            midHead.next = prev;
+            prev = midHead;
+            midHead = next;
+            i++;
+        }
+
+        /// Link with the third segment
+        firstTail.next = prev;
+        midTail.next = midHead;
+
+        return dummyNode.next;
+
+    }
 }
--- src/Leetcode/Q228_Summary_Ranges.java
+++ src/Leetcode/Q228_Summary_Ranges.java
@@ -1,7 +1,33 @@
 package Leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q228_Summary_Ranges {
+    public List<String> summaryRanges(int[] nums) {
+
+        int length = nums.length;
+
+        List<String> result = new ArrayList<String>(length);
+
+        for (int i = 0; i < length; i++) {
+            int start = nums[i];
+
+            while (i < length - 1 && nums[i] + 1 == nums[i + 1]) {
+                i++;
+            }
+
+            if (start != nums[i]) {
+                result.add(start + "->" + nums[i]);
+            } else {
+                result.add(start + "");
+            }
+        }
+
+        return result;
+
+    }
 }
