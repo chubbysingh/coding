928e025496c958d933cbb358e664e7524644f5f3
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-03-25 03:58:33 -0700

    Solutions

--- src/Leetcode/Q216_Combination_Sum_III.java
+++ src/Leetcode/Q216_Combination_Sum_III.java
@@ -1,7 +1,33 @@
 package Leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q216_Combination_Sum_III {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> curr = new ArrayList<Integer>();
+        helper(result, curr, k, 1, n);
+        return result;
+    }
+
+    public void helper(List<List<Integer>> result, List<Integer> curr, int k, int start, int sum){
+        if(sum<0){
+            return;
+        }
+
+        if(sum==0 && curr.size()==k){
+            result.add(new ArrayList<Integer>(curr));
+            return;
+        }
+
+        for(int i=start; i<=9; i++){
+            curr.add(i);
+            helper(result, curr, k, i+1, sum-i);
+            curr.remove(curr.size()-1);
+        }
+    }
 }
--- src/Leetcode/Q222_Count_Complete_Tree_Nodes.java
+++ src/Leetcode/Q222_Count_Complete_Tree_Nodes.java
@@ -1,7 +1,44 @@
 package Leetcode;
 
+import Leetcode.Util.TreeNode;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q222_Count_Complete_Tree_Nodes {
+    public int countNodes(TreeNode root) {
+        if(root==null)
+            return 0;
+
+        int left = getLeftHeight(root)+1;
+        int right = getRightHeight(root)+1;
+
+        if(left==right){
+            return (2<<(left-1))-1;
+        }else{
+            return countNodes(root.left)+countNodes(root.right)+1;
+        }
+    }
+
+    public int getLeftHeight(TreeNode n){
+        if(n==null) return 0;
+
+        int height=0;
+        while(n.left!=null){
+            height++;
+            n = n.left;
+        }
+        return height;
+    }
+
+    public int getRightHeight(TreeNode n){
+        if(n==null) return 0;
+
+        int height=0;
+        while(n.right!=null){
+            height++;
+            n = n.right;
+        }
+        return height;
+    }
 }
