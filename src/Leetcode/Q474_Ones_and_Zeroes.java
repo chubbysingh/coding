8eef3347982c3f945db02c95e71ad62273226b07
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-03-25 22:34:42 -0700

    Solutions

--- src/Leetcode/Q474_Ones_and_Zeroes.java
+++ src/Leetcode/Q474_Ones_and_Zeroes.java
@@ -4,4 +4,21 @@ package Leetcode;
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q474_Ones_and_Zeroes {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        for (String s : strs) {
+            int[] count = count(s);
+            for (int i=m;i>=count[0];i--)
+                for (int j=n;j>=count[1];j--)
+                    dp[i][j] = Math.max(1 + dp[i-count[0]][j-count[1]], dp[i][j]);
+        }
+        return dp[m][n];
+    }
+
+    public int[] count(String str) {
+        int[] res = new int[2];
+        for (int i=0;i<str.length();i++)
+            res[str.charAt(i) - '0']++;
+        return res;
+    }
 }
