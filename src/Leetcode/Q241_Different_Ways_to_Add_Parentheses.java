1b363342c7e3d0166b76b05ac187564f0a47bf45
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-03-25 22:08:32 -0700

    Solutions

--- src/Leetcode/Q241_Different_Ways_to_Add_Parentheses.java
+++ src/Leetcode/Q241_Different_Ways_to_Add_Parentheses.java
@@ -1,7 +1,41 @@
 package Leetcode;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q241_Different_Ways_to_Add_Parentheses {
+    public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> ret = new LinkedList<Integer>();
+        for (int i=0; i<input.length(); i++) {
+            if (input.charAt(i) == '-' ||
+                    input.charAt(i) == '*' ||
+                    input.charAt(i) == '+' ) {
+                String part1 = input.substring(0, i);
+                String part2 = input.substring(i+1);
+                List<Integer> part1Ret = diffWaysToCompute(part1);
+                List<Integer> part2Ret = diffWaysToCompute(part2);
+                for (Integer p1 :   part1Ret) {
+                    for (Integer p2 :   part2Ret) {
+                        int c = 0;
+                        switch (input.charAt(i)) {
+                            case '+': c = p1+p2;
+                                break;
+                            case '-': c = p1-p2;
+                                break;
+                            case '*': c = p1*p2;
+                                break;
+                        }
+                        ret.add(c);
+                    }
+                }
+            }
+        }
+        if (ret.size() == 0) {
+            ret.add(Integer.valueOf(input));
+        }
+        return ret;
+    }
 }
