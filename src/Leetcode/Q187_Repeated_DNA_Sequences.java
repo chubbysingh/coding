acb8d43e3e1f80d43eb3204f6d59e16328a92b2a
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-03-26 17:34:58 -0700

    Solutions

--- src/Leetcode/Q187_Repeated_DNA_Sequences.java
+++ src/Leetcode/Q187_Repeated_DNA_Sequences.java
@@ -1,24 +1,30 @@
 package Leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q187_Repeated_DNA_Sequences {
     public List<String> findRepeatedDnaSequences(String s) {
-        Map<String,Integer> map=new HashMap<String, Integer>();
-        Map<String,Integer> trash=new HashMap<String, Integer>();
-        List<String> ans=new ArrayList<String>();
+        Set<String> set=new HashSet<String>();
+        Set<String> repeated=new HashSet<String>();
+
+        List<String> ans=new LinkedList<String>();
+
         for (int i = 0; i < s.length()-9; i++) {
+
             String k=s.substring(i,i+10);
-            if (map.get(k)==null) {
-                map.put(k, 1);
-            }else if(trash.get(k)==null){
-                trash.put(k, 1);
+
+            if (!set.contains(k)) {
+                set.add(k);
+            }
+            // Its repeated. Check for duplication
+            else if(!repeated.contains(k)){
+                repeated.add(k);
                 ans.add(k);
             }
         }
