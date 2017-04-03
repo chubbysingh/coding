e0251c87694c741c7e07b8ffa6df7547c81ed5f1
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-04-02 22:42:32 -0700

    Solutions

--- src/Leetcode/Q211_Add_and_Search_Word_Data_structure_design.java
+++ src/Leetcode/Q211_Add_and_Search_Word_Data_structure_design.java
@@ -1,90 +1,75 @@
 package Leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q211_Add_and_Search_Word_Data_structure_design {
     private TrieNode root = new TrieNode();
-    private TrieNode dummyRoot = root;
 
     /** Adds a word into the data structure. */
     public void addWord(String word) {
         TrieNode curr = root;
-        int offset = 'a';
-        int i = 0;
-        for (i = 0; i < word.length(); i++) {
-            char character = word.charAt(i);
-            if (curr.children[character - offset] == null) {
-                curr.children[character - offset] =
-                        new TrieNode(character, i == word.length() - 1 ? true : false);
-            } else {
-                if (i == word.length() - 1) {
-                    curr.children[character - offset].leaf = true;
-                }
+        for (char c : word.toCharArray()) {
+            TrieNode node = curr.children.get(c);
+            if (node == null) {
+                node = new TrieNode();
+                curr.children.put(c, node);
             }
-
-            curr = curr.children[character - offset];
+            curr = node;
         }
+        curr.isLeaf = true;
     }
 
     /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
     public boolean search(String word) {
-        if (word == null || word.length() == 0) {
-            return false;
-        }
+        return dfsSearch(root.children, word, 0);
+    }
 
-        TrieNode curr = root;
-        int offset = 'a';
-        for (int i = 0; i < word.length(); i++) {
-            char character = word.charAt(i);
-            if (character != '.') {
-                if (curr.children[character - offset] == null) {
-                    return false;
-                }
-                curr = curr.children[character - offset];
-            } else {
-                // DFS
-                for (int j = 0; j < 26; j++) {
-                    if (curr.children[j] != null) {
-                        if (i == word.length() - 1 && curr.children[j].leaf) {
-                            root = dummyRoot;
-                            return true;
-                        }
-
-                        root = curr.children[j];
-                        if (search(word.substring(i + 1))) {
-                            root = dummyRoot;
-                            return true;
-                        }
-                    }
-                }
-                root = dummyRoot;
+    public boolean dfsSearch(Map<Character, TrieNode> children, String word, int start) {
+        if(start == word.length()){
+            if(children.size()==0)
+                return true;
+            else
                 return false;
-            }
         }
 
-        if (curr != null && curr.leaf == false) {
+        char c = word.charAt(start);
+
+        if(children.containsKey(c)){
+            if(start == word.length()-1 && children.get(c).isLeaf){
+                return true;
+            }
+
+            return dfsSearch(children.get(c).children, word, start+1);
+        }else if(c == '.'){
+            boolean result = false;
+            for(Map.Entry<Character, TrieNode> child: children.entrySet()){
+                if(start == word.length()-1 && child.getValue().isLeaf){
+                    return true;
+                }
+
+                //if any path is true, set result to be true;
+                if(dfsSearch(child.getValue().children, word, start+1)){
+                    result = true;
+                }
+            }
+
+            return result;
+        }else{
             return false;
         }
-
-        return true;
     }
 
     class TrieNode {
-        char val;
-        boolean leaf;
-        TrieNode[] children;
+        Map<Character, TrieNode> children;
+        boolean isLeaf;
 
         public TrieNode() {
-            this.val = '\0';
-            this.leaf = false;
-            this.children = new TrieNode[26];
-        }
-
-        public TrieNode(char val, boolean leaf) {
-            this.val = val;
-            this.leaf = leaf;
-            this.children = new TrieNode[26];
+            this.isLeaf = false;
+            this.children = new HashMap<Character, TrieNode>();
         }
     }
 }
