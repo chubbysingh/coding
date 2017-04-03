90367ed0f2c61b0d678f071b241f59090d5b97d0
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-04-03 01:58:09 -0700

    Solutions

--- src/Leetcode/Q435_Non_overlapping_Intervals.java
+++ src/Leetcode/Q435_Non_overlapping_Intervals.java
@@ -1,7 +1,38 @@
 package Leetcode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q435_Non_overlapping_Intervals {
+    public int eraseOverlapIntervals(Interval[] intervals) {
+        if (intervals.length == 0)  return 0;
+
+        Arrays.sort(intervals, new myComparator());
+        int end = intervals[0].end;
+        int count = 1;
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start >= end) {
+                end = intervals[i].end;
+                count++;
+            }
+        }
+        return intervals.length - count;
+    }
+
+    class myComparator implements Comparator<Interval> {
+        public int compare(Interval a, Interval b) {
+            return a.end - b.end;
+        }
+    }
+
+     public class Interval {
+         int start;
+         int end;
+         Interval() { start = 0; end = 0; }
+         Interval(int s, int e) { start = s; end = e; }
+     }
 }
--- src/Leetcode/Q467_Unique_Substrings_in_Wraparound_String.java
+++ src/Leetcode/Q467_Unique_Substrings_in_Wraparound_String.java
@@ -4,4 +4,31 @@ package Leetcode;
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q467_Unique_Substrings_in_Wraparound_String {
+    public int findSubstringInWraproundString(String p) {
+        // count[i] is the maximum unique substring end with ith letter.
+        // 0 - 'a', 1 - 'b', ..., 25 - 'z'.
+        int[] count = new int[26];
+
+        // store longest contiguous substring ends at current position.
+        int maxLengthCur = 0;
+
+        for (int i = 0; i < p.length(); i++) {
+            if (i > 0 && (p.charAt(i) - p.charAt(i - 1) == 1 || (p.charAt(i - 1) - p.charAt(i) == 25))) {
+                maxLengthCur++;
+            }
+            else {
+                maxLengthCur = 1;
+            }
+
+            int index = p.charAt(i) - 'a';
+            count[index] = Math.max(count[index], maxLengthCur);
+        }
+
+        // Sum to get result
+        int sum = 0;
+        for (int i = 0; i < 26; i++) {
+            sum += count[i];
+        }
+        return sum;
+    }
 }
