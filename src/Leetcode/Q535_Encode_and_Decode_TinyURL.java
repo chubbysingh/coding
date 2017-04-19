b5512a6ade9bd3435d38f25d740bbd967fccbf6d
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-04-19 00:58:21 -0700

    Solutions

--- src/Leetcode/Q535_Encode_and_Decode_TinyURL.java
+++ src/Leetcode/Q535_Encode_and_Decode_TinyURL.java
@@ -1,7 +1,36 @@
 package Leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by rbhatnagar2 on 3/5/17.
  */
 public class Q535_Encode_and_Decode_TinyURL {
+    Map<String, String> index = new HashMap<String, String>();
+    Map<String, String> revIndex = new HashMap<String, String>();
+    static String BASE_HOST = "http://tinyurl.com/";
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        if (revIndex.containsKey(longUrl)) return BASE_HOST + revIndex.get(longUrl);
+        String charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        String key = null;
+        do {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 6; i++) {
+                int r = (int) (Math.random() * charSet.length());
+                sb.append(charSet.charAt(r));
+            }
+            key = sb.toString();
+        } while (index.containsKey(key));
+        index.put(key, longUrl);
+        revIndex.put(longUrl, key);
+        return BASE_HOST + key;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return index.get(shortUrl.replace(BASE_HOST, ""));
+    }
 }
