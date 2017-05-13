a2a1423b9578361dc39463d35033e0e8e3bc7960
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-05-13 14:12:37 -0700

    Solutions

--- src/Leetcode/Q354_Russian_Doll_Envelopes.java
+++ src/Leetcode/Q354_Russian_Doll_Envelopes.java
@@ -1,7 +1,34 @@
 package Leetcode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q354_Russian_Doll_Envelopes {
+    public int maxEnvelopes(int[][] envelopes) {
+        if(envelopes == null || envelopes.length == 0
+                || envelopes[0] == null || envelopes[0].length != 2)
+            return 0;
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] arr1, int[] arr2) {
+                if (arr1[0] == arr2[0])
+                    return arr2[1] - arr1[1];
+                else
+                    return arr1[0] - arr2[0];
+            }
+        });
+        int dp[] = new int[envelopes.length];
+        int len = 0;
+        for(int[] envelope : envelopes){
+            int index = Arrays.binarySearch(dp, 0, len, envelope[1]);
+            if(index < 0)
+                index = -(index + 1);
+            dp[index] = envelope[1];
+            if(index == len)
+                len++;
+        }
+        return len;
+    }
 }
