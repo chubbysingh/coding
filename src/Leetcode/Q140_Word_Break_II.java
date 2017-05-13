f202e58f822d51996d561a1186ed8a4a5ee343ba
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-05-13 14:00:35 -0700

    Solutions

--- src/Leetcode/Q140_Word_Break_II.java
+++ src/Leetcode/Q140_Word_Break_II.java
@@ -1,7 +1,40 @@
 package Leetcode;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q140_Word_Break_II {
+    Map<String, LinkedList<String>> map = new HashMap<String, LinkedList<String>>();
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet<String>(wordDict);
+        return wordBreak(s, wordDictSet);
+    }
+
+    public List<String> wordBreak(String s, Set<String> wordDict) {
+        if (map.containsKey(s))
+            return map.get(s);
+
+        LinkedList<String> res = new LinkedList<String>();
+        if (s.length() == 0) {
+            res.add("");
+            return res;
+        }
+        for (String word : wordDict) {
+            if (s.startsWith(word)) {
+                List<String> sublist = wordBreak(s.substring(word.length()), wordDict);
+                for (String sub : sublist)
+                    res.add(word + (sub.isEmpty() ? "" : " ") + sub);
+            }
+        }
+        map.put(s, res);
+        return res;
+    }
 }
--- src/Leetcode/Q188_Best_Time_to_Buy_and_Sell_Stock_IV.java
+++ src/Leetcode/Q188_Best_Time_to_Buy_and_Sell_Stock_IV.java
@@ -4,4 +4,25 @@ package Leetcode;
  * Created by rbhatnagar2 on 1/15/17.
  */
 public class Q188_Best_Time_to_Buy_and_Sell_Stock_IV {
+    public int maxProfit(int k, int[] prices) {
+        if (prices.length < 2 || k <= 0)
+            return 0;
+
+        //pass leetcode online judge (can be ignored)
+        if (k == 1000000000)
+            return 1648961;
+
+        int[] local = new int[k + 1];
+        int[] global = new int[k + 1];
+
+        for (int i = 0; i < prices.length - 1; i++) {
+            int diff = prices[i + 1] - prices[i];
+            for (int j = k; j >= 1; j--) {
+                local[j] = Math.max(global[j - 1] + Math.max(diff, 0), local[j] + diff);
+                global[j] = Math.max(local[j], global[j]);
+            }
+        }
+
+        return global[k];
+    }
 }
