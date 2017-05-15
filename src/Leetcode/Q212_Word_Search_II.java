f3c2fdbdb6ab06422fc0c4e3a195ff89c4f14001
chubbysingh <Raghav_Bhatnagar@intuit.com>
2017-05-14 17:10:50 -0700

    Solutions

--- src/Leetcode/Q212_Word_Search_II.java
+++ src/Leetcode/Q212_Word_Search_II.java
@@ -1,5 +1,8 @@
 package Leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by rbhatnagar2 on 1/15/17.
  * <p>
@@ -10,4 +13,50 @@ package Leetcode;
  * The same letter cell may not be used more than once in a word.
  */
 public class Q212_Word_Search_II {
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> res = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs(board, i, j, root, res);
+            }
+        }
+        return res;
+    }
+
+    public void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
+        char c = board[i][j];
+        if (c == '#' || p.next[c - 'a'] == null) return;
+        p = p.next[c - 'a'];
+        if (p.word != null) {   // found one
+            res.add(p.word);
+            p.word = null;     // de-duplicate
+        }
+
+        board[i][j] = '#';
+        if (i > 0) dfs(board, i - 1, j, p, res);
+        if (j > 0) dfs(board, i, j - 1, p, res);
+        if (i < board.length - 1) dfs(board, i + 1, j, p, res);
+        if (j < board[0].length - 1) dfs(board, i, j + 1, p, res);
+        board[i][j] = c;
+    }
+
+    public TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String w : words) {
+            TrieNode p = root;
+            for (char c : w.toCharArray()) {
+                int i = c - 'a';
+                if (p.next[i] == null) p.next[i] = new TrieNode();
+                p = p.next[i];
+            }
+            p.word = w;
+        }
+        return root;
+    }
+
+    class TrieNode {
+        TrieNode[] next = new TrieNode[26];
+        String word;
+    }
 }
