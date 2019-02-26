afeb2169c8a6dbfefe30cb021f98fc5387ef6578
chubbysingh <raghav@alumni.usc.edu>
2019-02-26 12:07:46 -0800

    Iterative solution sameTree

--- src/Leetcode/Q009_Palindrome_Number.java
+++ src/Leetcode/Q009_Palindrome_Number.java
@@ -3,7 +3,18 @@ package Leetcode;
 /**
  * Created by rbhatnagar2 on 1/12/17.
  * <p>
- * https://leetcode.com/problems/palindrome-number/
+ * Determine whether an integer is a palindrome.
+ * An integer is a palindrome when it reads the same backward as forward.
+ *
+ * Example 1:
+ * Input: 121
+ * Output: true
+ *
+ * Example 2:
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121.
+ * From right to left, it becomes 121-. Therefore it is not a palindrome.
  */
 public class Q009_Palindrome_Number {
     public boolean isPalindrome(int x) {
--- src/Leetcode/Q100_Same_Tree.java
+++ src/Leetcode/Q100_Same_Tree.java
@@ -2,6 +2,9 @@ package Leetcode;
 
 import Leetcode.Util.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by rbhatnagar2 on 1/13/17.
  * <p>
@@ -9,11 +12,65 @@ import Leetcode.Util.TreeNode;
  * Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
  */
 public class Q100_Same_Tree {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    public boolean isSameTreeRecursive(TreeNode p, TreeNode q) {
         if (p == null && q == null)
             return true;
         else if (p == null || q == null)
             return false;
-        return ((p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        return ((p.val == q.val)
+                && isSameTreeRecursive(p.left, q.left)
+                && isSameTreeRecursive(p.right, q.right));
+    }
+
+    /**
+     * BFS on both the Trees
+     */
+    public boolean isSameTreeIterative(TreeNode p, TreeNode q) {
+        // Return true if both trees are empty
+        if (p == null && q == null)
+            return true;
+
+        // Return false if one is empty and other is not
+        else if (p == null || q == null)
+            return false;
+
+        // Create an empty queues for simultaneous traversals
+        Queue<TreeNode > q1 = new LinkedList<> ();
+        Queue<TreeNode> q2 = new LinkedList<>();
+
+        // Enqueue Roots of trees in respective queues
+        q1.add(p);
+        q2.add(q);
+
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            // Get front nodes and compare them
+            TreeNode n1 = q1.peek();
+            TreeNode n2 = q2.peek();
+
+            if (n1.val != n2.val)
+                return false;
+
+            // Remove front nodes from queues
+            q1.remove();
+            q2.remove();
+
+            /* Enqueue left children of both nodes */
+            if (n1.left != null && n2.left != null) {
+                q1.add(n1.left);
+                q2.add(n2.left);
+            }
+            else if (n1.left != null || n2.left != null) {
+                return false;
+            }
+
+            // Right child code (Similar to left child code)
+            if (n1.right != null && n2.right != null) {
+                q1.add(n1.right);
+                q2.add(n2.right);
+            }
+            else if (n1.right != null || n2.right != null)
+                return false;
+        }
+        return true;
     }
 }
