22d8bd8b52670094270231ac2d74c10319f6d565
chubbysingh <raghav@alumni.usc.edu>
2019-02-26 14:21:01 -0800

    Sqroot - cleanup code

--- src/Leetcode/Q069_Sqrt_x.java
+++ src/Leetcode/Q069_Sqrt_x.java
@@ -2,54 +2,24 @@ package Leetcode;
 
 /**
  * Created by rbhatnagar2 on 1/13/17.
+ *
+ * Since the return type is an integer,
+ * the decimal digits are truncated and only the integer part of the result is returned.
  */
 public class Q069_Sqrt_x {
-    public int mySqrt(int x) {
 
-        if (x <= 0)
-            return 0;
-
-        else if (x < 1)
-            return 1 / mySqrt(1 / x);
-
-        else if (x == 1)
-            return 1;
-
-        int left = 1, right = x / 2 + 1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-
-            int sq = mid * mid;
-            if (sq == x) {
-                return mid;
-            } else if (sq > x) {
-                right = mid;
-            } else {
-                if (mid + 1 > (x / (mid + 1))) {
-                    return mid;
-                }
-                left = mid + 1;
-            }
-        }
-        return -1;
-    }
-
-    public int mySqrtMethodTwo(int x) {
-
-        if (x == 0) {
-            return 0;
-        }
-
-        if (x < 0) {
+    public int sqrt(int x) {
+        if (x < 0)
             return -1;
-        }
+        if (x == 0)
+            return 0;
 
-        long lo = 0;
-        long hi = x / 2 + 1;
+        double lo = 0,
+               hi = x;
 
         while (lo <= hi) {
-            long mid = lo + (hi - lo) / 2;
-            long sq = mid * mid;
+            double mid = lo + (hi - lo) / 2;
+            double sq = mid * mid;
             if (sq == x) {
                 return (int) mid;
             }
@@ -64,6 +34,11 @@ public class Q069_Sqrt_x {
         return (int) hi;
     }
 
+    /**
+     * Sqrt with precision
+     * @param num
+     * @return
+     */
     public double sqrt(double num) {
         if (num < 0)
             return -1;
@@ -72,11 +47,13 @@ public class Q069_Sqrt_x {
         if (num < 1)
             return 1 / sqrt(1/num);
 
-        double lo = 0, hi = num;
+        double lo = 0,
+               hi = num;
+
         double precision = 0.00000000000001;
 
         while(true) {
-            double mid = lo + (hi-lo)/2;
+            double mid = lo + (hi-lo) / 2;
 
             double currPrec = Math.abs(mid*mid - num);
             if (currPrec < precision)
