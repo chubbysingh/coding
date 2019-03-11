e2a50e30303632ac8447763d70339a5daf564deb
chubbysingh <raghav@alumni.usc.edu>
2019-03-11 03:16:53 -0700

    Correcting solution

--- src/Leetcode/Q069_Sqrt_x.java
+++ src/Leetcode/Q069_Sqrt_x.java
@@ -9,29 +9,25 @@ package Leetcode;
 public class Q069_Sqrt_x {
 
     public int sqrt(int x) {
-        if (x < 0)
-            return -1;
         if (x == 0)
             return 0;
 
-        double lo = 0,
-               hi = x;
+        int left = 1, //to avoid NPE
+            right = x;
 
-        while (lo <= hi) {
-            double mid = lo + (hi - lo) / 2;
-            double sq = mid * mid;
-            if (sq == x) {
-                return (int) mid;
-            }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-            if (sq < x) {
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
+            if (mid == x/mid)
+                return mid;
+
+            else if (mid <= x / mid)
+                left = mid + 1;
+
+            else
+                right = mid - 1;
         }
-
-        return (int) hi;
+        return right;
     }
 
     /**
@@ -40,14 +36,13 @@ public class Q069_Sqrt_x {
      * @return
      */
     public double sqrt(double num) {
-        if (num < 0)
-            return -1;
-        if (num == 0)
-            return 0;
-        if (num < 1)
-            return 1 / sqrt(1/num);
+        if (num < 0) return -1;
 
-        double lo = 0,
+        if (num == 0) return 0;
+
+        if (num < 1) return 1 / sqrt(1/num);
+
+        double lo = 1,
                hi = num;
 
         double precision = 0.00000000000001;
